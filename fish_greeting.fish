a9106460507c73b24a0368153e5dedd91bfc0fde
aholowko <andrzej.holowko@gmail.com>
2016-01-20 13:17:51 +0100

    init

---
+++ fish_greeting.fish
@@ -0,0 +1,12 @@
+# Set global color styles, for example:
+#
+# function aholowko_error
+#   set_color -o red
+# end
+#
+# function aholowko_normal
+#   set_color normal
+#
+
+function fish_greeting
+end
